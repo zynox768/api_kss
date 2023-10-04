a8d03186c8ec08c4fee7f520ff79b5d603ef5b5f
Dibyajyoti Prakash <88896505+zynox768@users.noreply.github.com>
2023-10-05 00:36:52 +0530

    Update api.js4

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 443|80;
+const port = 443;
 const cors = require('cors');
 const bcrypt = require('bcrypt'); // Add bcrypt for password hashing
 const session = require('express-session');
